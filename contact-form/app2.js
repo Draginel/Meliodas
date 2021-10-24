2ca62103d679581eeabb569e5a41b19f9abd7ca3
Hassan Deghedy <93010080+Draginel@users.noreply.github.com>
2021-10-24 22:48:50 +0200

    Add files via upload

---
+++ contact-form/app2.js
@@ -0,0 +1,17 @@
+function change2() {
+    document.getElementById("click3").classList.add('isFocused');
+    document.getElementById("click4").classList.add('animationInitialized');
+
+}
+
+function change() {
+    document.getElementById("click").classList.add('isFocused');
+    document.getElementById("click2").classList.add('animationInitialized');
+
+}
+
+function change3() {
+    document.getElementById("click5").classList.add('isFocused');
+    document.getElementById("click6").classList.add('animationInitialized');
+
+}
